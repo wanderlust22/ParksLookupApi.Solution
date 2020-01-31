3c36a3cb1c7a72ea1121051c8e2b1f24601c6b9c
Student <example@example.com>
2020-01-31 13:30:40 -0800

    implement Swagger API documentation and set it to splash page

--- ParksLookupApi/Startup.cs
+++ ParksLookupApi/Startup.cs
@@ -5,6 +5,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Reflection;
+using System.IO;
 
 
 namespace ParksLookupApi
@@ -24,6 +28,29 @@ namespace ParksLookupApi
             services.AddDbContext<ParksLookupApiContext>(opt =>
                 opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Parks Lookup API",
+                    Description = "Lookup, add, and review state and national parks",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Will Quanstrom",
+                        Email = "wquanstr215@gmail.com",
+                        Url = new Uri("https://willquanstrom.com"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "The MIT license",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +65,19 @@ namespace ParksLookupApi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Parks Lookup Api V1");
+                c.RoutePrefix = "documentation";
+
+            });
+
 
             // app.UseHttpsRedirection();
             app.UseMvc();
